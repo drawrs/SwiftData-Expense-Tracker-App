10e1ada36d2ffac6cbce1033fe00ff2e9f08a4d0
drawrs <rizal.hilman67@gmail.com>
2024-05-23 21:52:13 +0700

    Initial Commit

---
+++ SwiftData Expense Tracker App/SwiftData_Expense_Tracker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_Expense_Tracker_AppApp.swift
+//  SwiftData Expense Tracker App
+//
+//  Created by Rizal Hilman on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_Expense_Tracker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
